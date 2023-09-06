42d18d2177c9dc04c846a1c53759e1ce246cffca
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2023-09-06 05:00:20 -0300

    user, login-register-logout

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('', include('apps.posts.urls')),
     path('', include('apps.contacto.urls')),
+    path('', include('django.contrib.auth.urls'))
 ]+static(settings.STATIC_URL,
 document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
